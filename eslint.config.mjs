64351222e745ee7b083be437ce4c5fda0b943026
Ting <hsiaoting1003@gmail.com>
2025-05-31 21:49:37 +0800

    eslint 設定

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,6 +1,27 @@
+// eslint 設定檔
 // @ts-check
-import withNuxt from './.nuxt/eslint.config.mjs'
+import withNuxt from './.nuxt/eslint.config.mjs';
 
 export default withNuxt(
-  // Your custom configs here
-)
+  {
+    // 加在這裡，只影響 .vue
+    files: ['**/*.vue'],
+    rules: {
+      // 關閉標籤時不需換行
+      'vue/html-closing-bracket-newline': 'off',
+      // 不限制每行屬性數，避免強制換行
+      'vue/max-attributes-per-line': 'off',
+    },
+  },
+  {
+    // 加在這裡，全域生效
+    rules: {
+      'no-console': 'warn', // 使用 console 時顯示警告提示
+      'no-var': 'error', // 禁止使用 var
+      'eqeqeq': 'error', // 使用嚴格相等運算子
+      'max-len': ['error', { code: 100 }], // 限制每行長度最多 100 個字元
+      'nuxt/nuxt-config-keys-order': 'off', // 關閉 Nuxt 配置鍵順序檢查
+      'vue/multi-word-component-names': 'off', // 關閉 Vue 組件名稱必須為多字詞的檢查
+    },
+  },
+);
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,8 +1,8 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: '2025-05-15',
+  modules: ['@nuxt/eslint', '@nuxt/icon', '@nuxt/image', '@nuxt/eslint'],
   devtools: { enabled: true },
-  modules: ['@nuxt/eslint', '@nuxt/icon', '@nuxt/image'],
   css: ['~/assets/css/main.css'],
   postcss: {
     plugins: {
@@ -10,4 +10,15 @@ export default defineNuxtConfig({
       autoprefixer: {},
     },
   },
-})
+  eslint: {
+    checker: true,
+    config: {
+      stylistic: {
+        indent: 2, // 使用 2 個空格縮排
+        semi: true, // 強制使用分號
+        quotes: 'single', // 強制使用單引號
+        commaDangle: 'always-multiline', // 多行時最後一個元素後有逗號
+      },
+    },
+  },
+});
