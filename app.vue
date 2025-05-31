285f0986d31506823e768e537aa122bbd1359ab4
Ting <hsiaoting1003@gmail.com>
2025-05-31 20:22:35 +0800

    建立 nuxt-practice 專案，安裝 Tailwind CSS 3

---
+++ app.vue
@@ -0,0 +1,7 @@
+<template>
+   <h1 class="text-3xl font-bold text-white bg-blue-300 underline rounded-lg p-4 m-4 flex justify-center">Hello Tailwind 測試!</h1>
+  <div>
+    <NuxtRouteAnnouncer />
+    <NuxtWelcome />
+  </div>
+</template>
