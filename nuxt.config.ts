47459be229a7aafac3fb63f2ada8de4f4268b6fa
Ting <hsiaoting1003@gmail.com>
2025-06-01 12:09:05 +0800

    安裝 Vuetify 3，調整 Tailwind CSS 檔名

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,9 +1,34 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
+import vuetify, { transformAssetUrls } from 'vite-plugin-vuetify';
+
 export default defineNuxtConfig({
   compatibilityDate: '2025-05-15',
-  modules: ['@nuxt/eslint', '@nuxt/icon', '@nuxt/image', '@nuxt/eslint'],
+  build: {
+    transpile: ['vuetify'],
+  },
+  modules: [
+    (_options, nuxt) => {
+      nuxt.hooks.hook('vite:extendConfig', (config) => {
+        // @ts-expect-error: vite config.plugins 的類型中未定義 vuetify plugin，但實際上是有效的
+        config.plugins.push(vuetify({ autoImport: true }));
+      });
+    },
+    '@nuxt/eslint',
+    '@nuxt/icon',
+    '@nuxt/image',
+    '@nuxt/eslint',
+  ],
+  vite: {
+    vue: {
+      template: {
+        transformAssetUrls,
+      },
+    },
+  },
   devtools: { enabled: true },
-  css: ['~/assets/css/main.css'],
+  css: [
+    '~/assets/css/tailwind.css',
+  ],
   postcss: {
     plugins: {
       tailwindcss: {},
